e28b92c4bf5a9b75fc690825233b9e113f72fea1
Michael Malak <michael.k.malak@gmail.com>
2019-11-23 10:43:34 +0200

    Modifying shapes

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,44 @@ class HelloRectangle extends StatelessWidget {
               color: Colors.red,
               shape: BoxShape.circle,
             ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 150.0,
+                  width: 150.0,
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black,
+                  ),
+                ),
+
+                Row(
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.greenAccent,
+                      ),
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 200.0,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
