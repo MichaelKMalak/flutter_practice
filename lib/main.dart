7ef5604729981b9f0db470189360586cee51d9a6
Michael Malak <michael.k.malak@gmail.com>
2019-11-23 12:08:33 +0200

    Removing the random background color Removing the refresh button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'dart:math' as math;
 
 void main() => runApp(MyApp());
 
@@ -8,12 +7,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Generate Random Words',
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-      ),
-      home: RandomWords(),
-    );
+        title: 'Generate Random Words',
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Random Words Generator'),
+          ),
+          body: Center(
+            child: RandomWords(),
+          ),
+        ));
   }
 }
 
@@ -23,34 +28,12 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  // ignore: unused_field
-  int _refreshTimes = 0;
-  void _refresh() {
-    setState(() {
-      _refreshTimes++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
-    return Scaffold(
-      backgroundColor:
-          Color((math.Random().nextDouble() * 0xFFFFFF).toInt() << 0)
-              .withOpacity(1.0),
-      appBar: AppBar(
-        title: Text('Random Words Generator'),
-      ),
-      body: Center(
-          child: Text(
-        wordPair.asPascalCase,
-        style: Theme.of(context).textTheme.display3,
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _refresh,
-        tooltip: 'Refresh',
-        child: Icon(Icons.refresh),
-      ),
+    return Text(
+      wordPair.asPascalCase,
+      style: Theme.of(context).textTheme.display3,
     );
   }
 }
