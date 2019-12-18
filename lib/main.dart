0897aaede63bdeca3470b8b05b4d6f84e513263b
12827629 <michael.k.malak@gmail.com>
2019-12-18 12:58:35 +0200

    create Image upload project

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Basic Shapes',
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Basic Shapes'),
-      ),
-      body: HelloRectangle(),
-    ),
-  ));
+class ImgUploadPage extends StatefulWidget {
+  @override
+  _ImgUploadPageState createState() {
+    return _ImgUploadPageState();
+  }
 }
 
-class HelloRectangle extends StatelessWidget {
+class _ImgUploadPageState extends State<ImgUploadPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.black,
-        height: 400.0,
-        width: 400.0,
-        child: Center(
-          child: Container(
-            height: 400.0,
-            width: 400.0,
-            decoration: new BoxDecoration(
-              color: Colors.red,
-              shape: BoxShape.circle,
-            ),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 150.0,
-                  width: 150.0,
-                  decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.black,
-                  ),
-                ),
-
-                Row(
-                  children: <Widget>[
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.greenAccent,
-                      ),
-                    ),
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.blueAccent,
-                      ),
-                    ),
-                    Container(
-                      height: 100.0,
-                      width: 200.0,
-                      color: Colors.black,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return SafeArea(
+        child: Scaffold(
+          body: Text("Test!"),
+        )
     );
   }
 }
